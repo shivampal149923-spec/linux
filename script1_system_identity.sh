3636f4bc322ae7058e9309fdcf40bc124792d9be
coder20282006 <bhavya2311006@gmail.com>
2026-03-30 18:22:14 +0530

    first commit

---
+++ script1_system_identity.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Script 1: System Identity Report
+# Author: Dhruv Chandwani
+# Course: Open Source Software
+
+# Variables
+STUDENT_NAME="Dhruv Chandwani"
+SOFTWARE_CHOICE="Python"
+
+# System information
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DATE=$(date)
+DISTRO=$(lsb_release -d | cut -f2)
+
+# Output
+echo "======================================"
+echo " Open Source Audit - $STUDENT_NAME"
+echo "======================================"
+
+echo "Chosen Software : $SOFTWARE_CHOICE"
+echo "Kernel Version  : $KERNEL"
+echo "Current User    : $USER_NAME"
+echo "Linux Distro    : $DISTRO"
+echo "System Uptime   : $UPTIME"
+echo "Current Date    : $DATE"
+
+echo ""
+echo "Linux is distributed under the GNU General Public License (GPL).
