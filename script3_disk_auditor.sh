19d9929b0c41b24049dfb5e88e620e4d41bec92b
shivampal149923-spec <shivampal149923@gmail.com>
2026-03-30 23:04:07 +0530

    Update script3_disk_auditor.sh

--- script3_disk_auditor.sh
+++ script3_disk_auditor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script 3: Disk and Permission Auditor
-# Author: Bhavya Manish Jain
+# Author: Shivam
 # Course: Open Source Software
 
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
