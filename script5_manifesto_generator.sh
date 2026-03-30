95bbd87c8197025f5cc89ead0dcdde21bb0ea6c2
shivampal149923-spec <shivampal149923@gmail.com>
2026-03-30 23:04:53 +0530

    Update script5_manifesto_generator.sh

--- script5_manifesto_generator.sh
+++ script5_manifesto_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script 5: Open Source Manifesto Generator
-# Author: Bhavya Manish Jain
+# Author: Shivam
 # Course: Open Source Software
 
 echo "Answer the following questions to generate your Open Source Manifesto."
