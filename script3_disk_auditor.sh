c56fe44d9c31243ebfa8d6eaebf643b249ab08a3
coder20282006 <bhavya2311006@gmail.com>
2026-03-30 18:28:10 +0530

    Update script3_disk_auditor.sh

--- script3_disk_auditor.sh
+++ script3_disk_auditor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script 3: Disk and Permission Auditor
-# Author: Dhruv Chandwani
+# Author: Bhavya Manish Jain
 # Course: Open Source Software
 
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
